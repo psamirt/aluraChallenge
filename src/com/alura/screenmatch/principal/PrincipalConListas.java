95e24b9681b2397ca6f0639febf6ce7e19f2aeba
psamirt <p_samir@hotmail.com>
2024-06-11 18:05:51 -0500

    primer commit

---
+++ src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,42 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+import com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua(9);
+        Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.evalua(6);
+        var peliculaDeBruno = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeBruno.evalua(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(miPelicula);
+        lista.add(otraPelicula);
+        lista.add(peliculaDeBruno);
+        lista.add(lost);
+
+        for (Titulo item: lista){
+            System.out.println(item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 2) {
+                System.out.println(pelicula);
+            }
+        }
+
+        ArrayList<String> listaDeArtistas = new ArrayList<>();
+        listaDeArtistas.add("Penelope cruz");
+        listaDeArtistas.add("Antonio Banderas");
+        listaDeArtistas.add("Ricardo arjona");
+
+        Collections.sort(listaDeArtistas);
+        System.out.println("Lista de artistas ordenada" + listaDeArtistas);
+    }
+}
